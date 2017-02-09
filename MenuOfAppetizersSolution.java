4e0a5064cb4ca87c301bf60d422f930412fc68d7
Pedro Villaroman <pedrovillaroman@gmail.com>
2017-02-09 15:51:10 -0800

    Modified Menu solution to use a recursive strategy I am more familiar with. However, we are getting duplicate lists. Need to fix

--- MenuOfAppetizersSolution.java
+++ MenuOfAppetizersSolution.java
@@ -1,6 +1,4 @@
-import java.io.*;
 import java.util.*;
-
 
 /** 
  * Problem is based off following xkcd:
@@ -36,33 +34,69 @@ public class MenuOfAppetizersSolution {
 		menu.put("Fries", 2.75);
 		menu.put("Fruit", 2.15);
 
-		ArrayList<Double> values = menu.values();
 
 		Set<String> items = menu.keySet();
 
-		// Converted into integers so it is easier to understand for me
+		// Converted into integers so it is easier to understand in calculations.
 		int priceAsInt = 1505;
 		int[] menuItemsPrices = {580, 420, 355, 335, 275, 215};
-		
 
-		HashMap<Double, Integer> numberOfWaysForEachPrice = new HashMap<Double, Integer>();
+		System.out.println("~~NEW WAY~~");
 
-		int[] counts = new int[menuItemsPrices.length];
+		ArrayList<Integer> counts = new ArrayList<Integer>();
+		for (int i = 0; i < menuItemsPrices.length; i++) {
+			counts.add(0);
+		}
+		ArrayList<ArrayList<Integer>> listResult = new ArrayList<ArrayList<Integer>>();
+		getNumberOfEachItem(menuItemsPrices, counts, priceAsInt, listResult);
+		printListResult(listResult, items);
+
+		System.out.println("~~OLD WAY~~");
+
+		int[] counts1 = new int[menuItemsPrices.length];
 		ArrayList<ArrayList<Integer>> lists = new ArrayList<ArrayList<Integer>>();
-		getNumberOfEachItem(menuItemsPrices, counts, 0, priceAsInt, lists);
+		getNumberOfEachItem2(menuItemsPrices, counts1, 0, priceAsInt, lists);
+		printListResult(lists, items);
+
+	}
+
+	private static void printListResult(ArrayList<ArrayList<Integer>> lists, Set<String> items) {
 		for (ArrayList<Integer> list : lists) {
 			int index = 0;
-			for (String item : menu.keySet()) {
+			for (String item : items) {
 				System.out.print(item + ": " + list.get(index) + ". ");
 				index++;
 			}
 			System.out.println();
 		}
-
-
 	}
 
-	public static void getNumberOfEachItem(int[] menu, int[] counts,
+	public static void getNumberOfEachItem(int[] menu, ArrayList<Integer> counts, int totalAmount, 
+		ArrayList<ArrayList<Integer>> listOfCounts) {
+
+		if (totalAmount == 0) {
+			listOfCounts.add(counts);
+			return;
+		}
+
+		// We want to loop through the menu items and check price satisfies amount we have
+		for (int i = 0; i < menu.length; i++) {
+			if (totalAmount - menu[i] >= 0) {
+				counts.set(i, counts.get(i)+1);
+				getNumberOfEachItem(menu, new ArrayList<Integer>(counts), totalAmount - menu[i], 
+					listOfCounts);
+				counts.set(i, counts.get(i)-1);
+			}
+		}
+		
+	}
+
+	/**
+	 * Algorithm does not utilize Dynamic Programming and Memoization as opposed to
+	 * getting the number of ways to reach a certain amount. This is so we can
+	 * attain the possible combinations until a certain amount
+	 */
+	public static void getNumberOfEachItem2(int[] menu, int[] counts,
 		int startIndex, int totalAmount, ArrayList<ArrayList<Integer>> lists) {
 
 		if (startIndex >= menu.length) {
@@ -77,13 +111,13 @@ public class MenuOfAppetizersSolution {
 		if (startIndex == menu.length-1) {
 			if (totalAmount%menu[startIndex] == 0) {
 				counts[startIndex] = totalAmount/menu[startIndex];
-				getNumberOfEachItem(menu, counts, startIndex + 1, 0, lists);
+				getNumberOfEachItem2(menu, counts, startIndex + 1, 0, lists);
 			}
 		}
 		else {
 			for (int i = 0; i <= totalAmount/menu[startIndex]; i++) {
 				counts[startIndex] = i;
-				getNumberOfEachItem(menu, counts, startIndex + 1, totalAmount - menu[startIndex]*i, lists);
+				getNumberOfEachItem2(menu, counts, startIndex + 1, totalAmount - menu[startIndex]*i, lists);
 			}
 		}
 		return;
